d676cf409d5895a045fd08183d23fd991ae4e68a
olaoluwa <itzdonlexy@gmail.com>
2022-07-11 14:35:53 -0700

    Changes to be committed:

---
+++ java/index.js
@@ -0,0 +1,20 @@
+ //animate_navigation_bar
+
+ window.addEventListener('scroll', function (){
+    let header = document.querySelector('nav');
+    let windowPosition = window.scrollY>0;
+    header.classList.toggle('animate-nav', windowPosition);
+});
+
+// mediaa-navigation
+const  navSlide = () => {
+    const burger =document.querySelector('.burger');
+    const navLink =document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        navLink.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide()
